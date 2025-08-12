12085f13d605ddbeaa3e43e87a5d223b13b4815d
timursamanchi <147069682+timursamanchi@users.noreply.github.com>
2025-08-12 20:32:52 +0100

    modified terraform

--- terraform/01-providers.tf
+++ terraform/01-providers.tf
@@ -29,6 +29,7 @@ terraform {
     }
   }
 }
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
